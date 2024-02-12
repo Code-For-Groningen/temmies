049ef0ea7fd13f6add573aa25241bdee9201ec77
Boyan <boyan@confest.im>
2024-02-12 13:05:52 +0100

    added config

---
+++ src/Year.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import selenium
+
+# Class to represent an academic year
+#  
+class Year:
+  def __init__
+  
---
+++ src/config.py
@@ -0,0 +1,5 @@
+username = "<s-number>"
+password = "<password>"
+
+if __name__ == "__main__":
+  print("Do not run this module like this. Just set the values.")
