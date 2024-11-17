6a63abf7aecebe2925f87fdb6fc6c3581d6b18c2
Boyan <36108495+confestim@users.noreply.github.com>
2024-11-17 19:50:19 +0100

    Expose only Themis on package-level

--- temmies/__init__.py
+++ temmies/__init__.py
@@ -1,7 +1,3 @@
 from .themis import Themis
-from .year import Year
-from .course import Course
-from .exercise_group import ExerciseGroup
-from .submission import Submission
 
-__all__ = ['Themis', 'Year', 'Course', 'ExerciseGroup', 'Submission']
+__all__ = ["Themis"]
