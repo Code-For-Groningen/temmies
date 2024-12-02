aa7b91de0dcf79b1591661616cc7974e014a06ff
Boyan <boyan@confest.im>
2024-12-02 17:07:56 +0100

    New version fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     
 setup(
     name="temmies",
-    version="1.2.1",
+    version="1.2.12",
     packages=find_packages(),
     description="A wrapper for the Themis website",
     long_description=l_description,
