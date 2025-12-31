a702d1e36798b643632ab180015bdcd01b31061b
Boyan <36108495+confestim@users.noreply.github.com>
2025-12-31 15:02:49 +0200

    Attempting to fix auto releases

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     author="Boyan K.",
     author_email="boyan@confest.im",
     license="GPLv3",
+    license_files=("LICENSE",),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
